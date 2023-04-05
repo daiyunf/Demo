0f291754e218fa9697505729672214ffdc3f03b4
daiyunfeng <484836894@qq.com>
2023-04-05 22:10:00 +0800

    初次提交

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello git!");
         System.out .println("hello git2!");
+        System.out.println("hello git3!");
     }
 }
